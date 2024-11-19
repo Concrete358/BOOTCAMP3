fd76027c8d8a3e40c83fbaa3093841361ba6afda
Anton Krachkouski <concrete358@gmail.com>
2024-11-19 10:08:31 +0100

    Checkstyle violation

--- src/main/java/com/example/controller/SimpleController.java
+++ src/main/java/com/example/controller/SimpleController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 class SimpleController {
 
+    public String str = "";
+
     @Value("${DEVOPS}")
     private String devopsVariable;
 
